ec0b6bdbf6b5ee82d2e1468c70f760bb18db89b5
jmubago <jmubagosotomayor@gmail.com>
2019-05-24 12:06:53 +0200

    loop cards html, open challenge dialogs

--- src/app/components/challenges/challengedisplay/challengedisplay.component.ts
+++ src/app/components/challenges/challengedisplay/challengedisplay.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 
 import { ChallengesService } from '../../../services/challenges.service';
+import { CashMachineDialogComponent } from '../../dialog/cash-machine-dialog/cash-machine-dialog.component';
+import { NonRepeatingCharacterDialogComponent } from '../../dialog/non-repeating-character-dialog/non-repeating-character-dialog.component';
+import { RomanNumbersDialogComponent } from '../../dialog/roman-numbers-dialog/roman-numbers-dialog.component';
+import { ScaleBalancingDialogComponent } from '../../dialog/scale-balancing-dialog/scale-balancing-dialog.component';
 
 @Component({
   selector: 'app-challengedisplay',
@@ -10,12 +15,50 @@ import { ChallengesService } from '../../../services/challenges.service';
 export class ChallengedisplayComponent implements OnInit {
   public challengeCards: any;
 
-  constructor(private challenge: ChallengesService) { }
+  constructor(private challenge: ChallengesService, public dialog: MatDialog) { }
 
   ngOnInit() {
     this.challengeCards = this.challenge.getChallengeDisplay();
-    console.log(typeof(this.challengeCards), this.challengeCards);
   }
 
+  /**
+   * Opens the appropiate dialog
+   * @param data card object with properties
+   */
+  public callDialog(data: any){
+    this[data.dialog](data);
+  }
+
+  /**
+   * Opens cash machine challenge dialog
+   * @param data card object with properties
+   */
+  public openCashMachineDialog(data: any){
+
+    const createDialog = this.dialog.open(CashMachineDialogComponent, {
+      data: {
+        title: data.title,
+        description: data.dialogDescription
+      }
+    });
+
+    createDialog.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      // this.animal = result;
+    });
+
+  }
+
+  public openNonRepeatingCharacterDialog(){
+    console.log('cash machine dialog');
+  }
+
+  public openRomanNumbersDialog(){
+    console.log('cash machine dialog');
+  }
+
+  public openScaleBalancingDialog(){
+    console.log('cash machine dialog');
+  }
 
 }

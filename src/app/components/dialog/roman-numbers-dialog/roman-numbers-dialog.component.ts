6286b11d415e6b14479196196613bab74fb144ea
jmubago <jmubagosotomayor@gmail.com>
2019-05-27 16:37:46 +0200

    working roman numbers challenge

--- src/app/components/dialog/roman-numbers-dialog/roman-numbers-dialog.component.ts
+++ src/app/components/dialog/roman-numbers-dialog/roman-numbers-dialog.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewEncapsulation } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { FormControl, Validators } from '@angular/forms';
+
+import { ChallengesService } from '../../../services/challenges.service';
 
 @Component({
   selector: 'app-roman-numbers-dialog',
   templateUrl: './roman-numbers-dialog.component.html',
-  styleUrls: ['./roman-numbers-dialog.component.css']
+  styleUrls: ['./roman-numbers-dialog.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class RomanNumbersDialogComponent implements OnInit {
 
-  constructor() { }
+  public title = new FormControl('', [Validators.required]);
+  public results: string;
+
+  constructor(public dialogRef: MatDialogRef<RomanNumbersDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private challenge: ChallengesService) { }
 
   ngOnInit() {
   }
 
+  /**
+   * Receives challege results data
+   * @param data 
+   */
+  public getChallenge (data){
+    if(!data.text || data.text === " "){
+      this.results = 'Please, enter a natural number';
+      return;
+    }
+    let text = data.text;
+    this.challenge.romanNumbersChallenge(text)
+    .subscribe(item => {
+        this.results = item.result;
+    })
+  }
+
+   /**
+   * Method for closing the dialog
+   */
+  onCloseClick(): void {
+    this.dialogRef.close();
+  }
 }

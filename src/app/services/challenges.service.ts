cee3d7919798904cf8aeb31fe3b7ab59658650c6
jmubago <jmubagosotomayor@gmail.com>
2019-05-27 16:38:45 +0200

    added http post request

--- src/app/services/challenges.service.ts
+++ src/app/services/challenges.service.ts
@@ -47,22 +47,16 @@ public getChallengeDisplay(){
       dialog: 'openScaleBalancingDialog',
       github: 'https://github.com/jmubago/CodeChallengesPortfolio_Server/blob/master/server/challenges/ScaleBalancingChallenge.js',
       dialogDescription: "Given two arrays with integers, the first being the weights on a balance scale (left, and right sides) and the secon array being a list of available weigts as positive integers. Your goal is to determine if you can balance the scale by using the least amount of weights from the list, but using at most only 2 weights. For example array1 = [5,9] and array2 = [1,2,6,7] then this means there is a balance scale with a weight of 5 on the left side and 9 on the right side. It is in fact possible to balance this scale by adding a 6 to the left side from the list of weights and adding a 2 to the right side. Both scales will now equal 11 and they are perfectly balanced. Your program should return a comma separated string of the weights that were used from the list in ascending order, do for this example your program should return the string '2,6'. There will only ever be one unique solution and the list of avilable weights will not be empty. It is also possible to add two weights to only one side of the scale to balance it. If it is not possible to balance the scale then your program should return the string 'not possible'"
-    },
-    {
-      title: 'Sample sample',
-      subtitle: 'Coding challenge 5',
-      cardDescription: 'sample',
-      dialog: 'openCashMachineDialog'
     }
   ];
   return challenge;
 }
 
-/**
- * HTTP post request to get fetch data for cash machine challenge
- * @param pp Purchase price
- * @param ch Cash
- */
+  /**
+   * HTTP post request to get fetch data for cash machine challenge
+   * @param pp Purchase price
+   * @param ch Cash
+   */
   public cashMachineChallenge(pp: number, ch: number): Observable<any>{
       let url = this.getQuery('cashmachine');
       let body = {
@@ -74,6 +68,10 @@ public getChallengeDisplay(){
       return this.http.post(url, body, headers);
   }
 
+  /**
+   * HTTP post request to get fetch data for non repeating character challenge
+   * @param text text
+   */
   public nonRepeatingCharacter(text: string): Observable<any>{
     let url = this.getQuery('nonrepeatingcharacter');
       let body = {
@@ -83,6 +81,37 @@ public getChallengeDisplay(){
 
       return this.http.post(url, body, headers);
   }
+
+  /**
+   * HTTP post request to get fetch data for roman numbers challenge
+   * @param num number
+   */
+  public romanNumbersChallenge(num: string): Observable<any>{
+    let url = this.getQuery('romannumbers');
+      let body = {
+        number: num
+      };
+      let headers = this.getHeaders();
+
+      return this.http.post(url, body, headers);
+  }
+
+  /**
+   * HTTP post request to get fetch data for scale balancing challenge
+   * @param num number
+   */
+  public scaleBalancingChallenge(ws: any[], w: any[]): Observable<any>{
+    let url = this.getQuery('scalebalancing');
+      let body = {
+        weightScale: ws,
+        weights: w
+      };
+      let headers = this.getHeaders();
+
+      return this.http.post(url, body, headers);
+  }
+
+
 
   // ## Private Methods
 

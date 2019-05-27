17171579ed701d898b260ba452c5eca7472d4645
jmubago <jmubagosotomayor@gmail.com>
2019-05-27 16:35:44 +0200

    deleted components routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { ChallengedisplayComponent } from './components/challenges/challengedisplay/challengedisplay.component';
-import { CashMachineChallengeComponent } from './components/challenges/cash-machine-challenge/cash-machine-challenge.component';
-import { NonRepeatingCharacterChallengeComponent } from './components/challenges/non-repeating-character-challenge/non-repeating-character-challenge.component';
-import { RomanNumbersChallengeComponent } from './components/challenges/roman-numbers-challenge/roman-numbers-challenge.component';
-import { ScaleBalancingChallengeComponent } from './components/challenges/scale-balancing-challenge/scale-balancing-challenge.component';
 import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
@@ -21,22 +17,6 @@ const routes: Routes = [
   {
     path: 'challenge',
     component: ChallengedisplayComponent
-  },
-  {
-    path: 'challenge/cashmachine',
-    component: CashMachineChallengeComponent
-  },
-  {
-    path: 'challenge/nonrepeatingcharacter',
-    component: NonRepeatingCharacterChallengeComponent
-  },
-  {
-    path: 'challenge/romannumbers',
-    component: RomanNumbersChallengeComponent
-  },
-  {
-    path: 'challenge/scalebalancing',
-    component: ScaleBalancingChallengeComponent
   },
   {
     path: '',
